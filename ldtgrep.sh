7fa17a362f7dc52815ae189b97ff8d18ff1fe870
Christian Weiß <weiss-christian@hotmail.de>
2019-10-06 19:37:21 +0200

    Erstes Script zum suchen nach Werten in LDT-Feldern.

---
+++ ldtgrep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z $1 ]
+    then
+        echo "Keine Argumente nagegeben!!!";
+        echo "Hilfe:";
+        echo ". ldtgrep <Verzeichnis> <Feldkennung> [sort]";
+        echo "";
+        exit 1;
+fi;
+
+if [ $3 == "sort" ]
+    then
+        grep -rE --include "*.ldt" "^\d{3}$2.*$" $1 | sort -u;
+    else
+        grep -rE --include "*.ldt" "^\d{3}$2.*$" $1;
+fi;
+
+exit 0;
